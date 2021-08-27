442878c0616bb6bac1831764675ce508f2995e67
wlilianhua <wlilianhua@163.com>
2021-08-27 13:26:31 +0800

    增加解析post data

--- app.js
+++ app.js
@@ -2,6 +2,34 @@ const qs = require('querystring');
 const handleBlogRouter = require('./src/router/blog');
 const handleUserRouter = require('./src/router/user');
 
+const getPostData = (req) => {
+  const promise = new Promise((resolve, reject) => {
+    if (req.method !== 'POST') {
+      resolve({});
+      return;
+    }
+
+    if (req.headers['content-type'] !== 'application/json') {
+      resolve({});
+      return;
+    }
+
+    let postData = '';
+    req.on('data', (chunk) => {
+      postData += chunk.toString();
+    });
+    req.on('end', () => {
+      if (!postData) {
+        resolve({});
+        return;
+      }
+      resolve(JSON.parse(postData));
+    });
+  });
+
+  return promise;
+};
+
 const serverHandle = (req, res) => {
   res.setHeader('Content-Type', 'application/json');
   const url = req.url;
@@ -10,24 +38,28 @@ const serverHandle = (req, res) => {
   // 处理query
   req.query = qs.parse(url.split('?')[1]);
 
-  // 处理blog路由数据
-  const blogData = handleBlogRouter(req, res);
-  if (blogData) {
-    res.end(JSON.stringify(blogData));
-    return;
-  }
+  getPostData(req).then((postData) => {
+    req.body = postData;
 
-  // 处理user路由数据
-  const userData = handleUserRouter(req, res);
-  if (userData) {
-    res.end(JSON.stringify(userData));
-    return;
-  }
+    // 处理blog路由数据
+    const blogData = handleBlogRouter(req, res);
+    if (blogData) {
+      res.end(JSON.stringify(blogData));
+      return;
+    }
 
-  //返回404
-  res.writeHead(404, { 'Content-type': 'text/plain' });
-  res.write('404 Not Found\n');
-  res.end();
+    // 处理user路由数据
+    const userData = handleUserRouter(req, res);
+    if (userData) {
+      res.end(JSON.stringify(userData));
+      return;
+    }
+
+    //返回404
+    res.writeHead(404, { 'Content-type': 'text/plain' });
+    res.write('404 Not Found\n');
+    res.end();
+  });
 };
 
 module.exports = serverHandle;
